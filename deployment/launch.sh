fb66b62a0a33be5ab46dce56dc413a491bb18e64
Boyu-Li <liboyu816@gmail.com>
2019-05-13 20:25:28 +1000

    Crawling update

--- deployment/launch.sh
+++ deployment/launch.sh
@@ -1,3 +1,1 @@
-sudo rm host_list.json
-sudo rm ansible_playbooks/hosts
-python launch_instance_boto.py nectar_key.json
+sudo rm host_list.json;sudo rm ansible_playbooks/hosts;python3 launch_instance_boto.py nectar_key.json;
